1f543e312c82676093486b03008aa8bcf2d291ac
SekiuchiHibiki <sekiuchi.hibiki@gmail.com>
2024-09-30 01:32:03 +0900

    initial commit

---
+++ src/test/java/com/example/shift_server/ShiftServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shift_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
